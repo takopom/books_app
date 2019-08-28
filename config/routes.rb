91d0c46a309c314875b2de3b5daf1a159bd21649
takopom <ymc722@gmail.com>
2019-08-28 10:20:20 +0900

    URLパラメータで言語を判定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(/:locale)", locale: /en|ja/ do
+    resources :books
+  end
+
+  get "/:locale" => "books#index"
 end
