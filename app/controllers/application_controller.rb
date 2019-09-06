acc382ce34475814b74063486bd7e360cab17a87
takopom <ymc722@gmail.com>
2019-09-06 14:06:03 +0900

    郵便番号などのパラメータを許可するように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     { locale: I18n.locale }
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :bio])
   end
 end
