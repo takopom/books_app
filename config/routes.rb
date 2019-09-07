13fb4d7d83436cc664cc382c4be2deada42570aa
takopom <ymc722@gmail.com>
2019-09-07 10:02:31 +0900

    user_rootのパスを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   scope "(/:locale)", locale: /en|ja/ do
     devise_for :users
     resources :books
+    get "/books" => "books#index", as: :user_root
   end
 
   get "/:locale" => "books#index"
-  get "/:locale/books" => "books#index", as: :user_root
 end
