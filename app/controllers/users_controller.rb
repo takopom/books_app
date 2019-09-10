2d2de149f3d9a77bcfdb4feee2ad0bbd5462a6fa
takopom <ymc722@gmail.com>
2019-09-10 15:36:35 +0900

    rubocopの指摘箇所を対応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
@@ -5,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
