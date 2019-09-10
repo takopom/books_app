2e24707870339d59ce8d45805d19189f414637c6
takopom <ymc722@gmail.com>
2019-09-10 15:33:41 +0900

    ユーザプロフィール表示用のViewとContorolerを追加

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class RegistrationsController < Devise::RegistrationsController
+  protected
+    def after_update_path_for(resource)
+      user_path(resource)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@
 
 Rails.application.routes.draw do
   scope "(/:locale)", locale: /en|ja/ do
-    devise_for :users
+    devise_for :users, controllers: { registrations: "registrations" }
     resources :books
+    resources :users, only: [:show]
     get "/books" => "books#index", as: :user_root
   end
 
