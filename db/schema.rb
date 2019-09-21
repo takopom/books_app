2967667e2eee0b0da9ed52bc554911d3670cd256
takopom <ymc722@gmail.com>
2019-09-21 22:01:51 +0900

    OmniAuthで利用するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +9,9 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 2019_09_03_203400) do
+
+ActiveRecord::Schema.define(version: 2019_09_16_014945) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -32,7 +32,11 @@ ActiveRecord::Schema.define(version: 2019_09_03_203400) do
     t.string "zipcode"
     t.string "address"
     t.string "bio"
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
