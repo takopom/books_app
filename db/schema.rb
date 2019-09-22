2f8237f52df3ee1adb005f03a15d28f1d8ce002e
takopom <ymc722@gmail.com>
2019-09-22 22:40:11 +0900

    不要なカラムをUsersから削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_014945) do
+ActiveRecord::Schema.define(version: 2019_09_22_133836) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_09_16_014945) do
     t.string "bio"
     t.string "provider"
     t.string "uid"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
