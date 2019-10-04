5dbe06345a75c5fda0a719c40d245a5a94a02a6e
takopom <ymc722@gmail.com>
2019-10-04 12:04:28 +0900

    コメントのDBテーブルを修正

--- db/migrate/20191003024221_create_comments.rb
+++ db/migrate/20191003024221_create_comments.rb
@@ -3,9 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :content
       t.references :user, foreign_key: true
-      t.bigint :commentable_id
-      t.string :commentable_type
-
+      t.references :commentable, polymorphic: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -47,10 +47,11 @@ ActiveRecord::Schema.define(version: 2019_10_03_024221) do
   create_table "comments", force: :cascade do |t|
     t.string "content"
     t.integer "user_id"
-    t.bigint "commentable_id"
     t.string "commentable_type"
+    t.integer "commentable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
