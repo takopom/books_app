ebe6e8bf739ee7b84d94238a6c123e417144051a
takopom <ymc722@gmail.com>
2019-10-09 14:56:52 +0900

    フォロワーの投稿一覧の表示ロジックを修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,17 +2,6 @@
 
 class HomesController < ApplicationController
   def show
-    @posts = []
-    relationships = Relationship.includes(:user).where(user_id: current_user.id)
-    relationships.each do |relationship|
-      user = User.where(id: relationship.followed_id).limit(1).first
-      books = Book.where(user_id: relationship.followed_id)
-      @posts.push(
-        user_id: user.id,
-        name: user.name,
-        avatar: user.avatar,
-        books: books
-      )
-    end
+    @followings = User.find(current_user.id).following
   end
 end
